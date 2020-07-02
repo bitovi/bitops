dee7eb1e7b359aa07fdca81df95bec353c8d9a8d
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2020-07-02 14:09:17 -0500

    unwrap ansible from function

--- scripts/ansible/ansible_install_playbooks.sh
+++ scripts/ansible/ansible_install_playbooks.sh
@@ -2,24 +2,21 @@
 
 set -e  
 
-function run_ansible_playbooks() {
-    path=""
 
-    if [ -z "$ANSIBLE_DIRECTORY" ]
-    then 
-        echo "Ansible directory not set. Using default directory."
-        path="$DEPLOYMENT_DIR/$CURRENT_ENVIRONMENT/ansible"
-    else
-        path="$CURRENT_ENVIRONMENT/$ANSIBLE_DIRECTORY/"
-        echo "Using provided Ansible directory: $path"
-    fi
-    
-    echo "Using Ansible Path: $path"
-    for playbook in $(ls $path/*.yaml || ls $path/*.yml)
-    do 
-        echo "Executing playbook: $playbook"
-        /root/.local/bin/ansible-playbook $playbook
-    done
-}
+path=""
 
-run_ansible_playbooks
+if [ -z "$ANSIBLE_DIRECTORY" ]
+then 
+    echo "Ansible directory not set. Using default directory."
+    path="$DEPLOYMENT_DIR/$CURRENT_ENVIRONMENT/ansible"
+else
+    path="$CURRENT_ENVIRONMENT/$ANSIBLE_DIRECTORY/"
+    echo "Using provided Ansible directory: $path"
+fi
+
+echo "Using Ansible Path: $path"
+for playbook in $(ls $path/*.yaml || ls $path/*.yml)
+do 
+    echo "Executing playbook: $playbook"
+    /root/.local/bin/ansible-playbook $playbook
+done
