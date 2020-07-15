1058393a1696e7ae41027e87ba15b96ea8ed3d07
dylan-bitovi <dylan@bitovi.com>
2020-07-15 15:50:35 -0400

    changed bitops script dir formatting bitops-before-deploy -> bitops.before-deploy

--- scripts/ansible/ansible_install_playbooks.sh
+++ scripts/ansible/ansible_install_playbooks.sh
@@ -4,14 +4,14 @@ set -ex
 
 PLUGIN_DIR="$ENVROOT/ansible"
 
-if [ -d "$PLUGIN_DIR/bitops-before-deploy.d/" ];then
-    BEFORE_DEPLOY=$(ls $PLUGIN_DIR/bitops-before-deploy.d/)
+if [ -d "$PLUGIN_DIR/bitops.before-deploy.d/" ];then
+    BEFORE_DEPLOY=$(ls $PLUGIN_DIR/bitops.before-deploy.d/)
     if [[ -n ${BEFORE_DEPLOY} ]];then
         echo "Running Before Deploy Scripts"
-        END=$(ls $PLUGIN_DIR/bitops-before-deploy.d/*.sh | wc -l)
+        END=$(ls $PLUGIN_DIR/bitops.before-deploy.d/*.sh | wc -l)
         for ((i=1;i<=END;i++)); do
-            if [[ -x "$PLUGIN_DIR/bitops-before-deploy.d/$i.sh" ]]; then
-                /bin/bash -x $PLUGIN_DIR/bitops-before-deploy.d/$i.sh
+            if [[ -x "$PLUGIN_DIR/bitops.before-deploy.d/$i.sh" ]]; then
+                /bin/bash -x $PLUGIN_DIR/bitops.before-deploy.d/$i.sh
             else
                 echo "Before deploy script is not executible. Skipping..."
             fi
