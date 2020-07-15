d98fbfbdab82c8502f4da7fb850540a815a424e3
dylan-bitovi <dylan@bitovi.com>
2020-07-15 17:51:30 -0400

    reverting changes, unnecessary

--- scripts/ansible/ansible_install_playbooks.sh
+++ scripts/ansible/ansible_install_playbooks.sh
@@ -1,29 +1,12 @@
 #!/usr/bin/env bash
 
-set -ex
-
-PLUGIN_DIR="$ENVROOT/ansible"
-
-if [ -d "$PLUGIN_DIR/bitops.before-deploy.d/" ];then
-    BEFORE_DEPLOY=$(ls $PLUGIN_DIR/bitops.before-deploy.d/)
-    echo $BEFORE_DEPLOY
-    if [[ -n ${BEFORE_DEPLOY} ]];then
-        echo "Running Before Deploy Scripts"
-        END=$(ls $PLUGIN_DIR/bitops.before-deploy.d/*.sh | wc -l)
-        for ((i=1;i<=END;i++)); do
-            if [[ -x "$PLUGIN_DIR/bitops.before-deploy.d/$i.sh" ]]; then
-                /bin/bash -x $PLUGIN_DIR/bitops.before-deploy.d/$i.sh
-            else
-                echo "Before deploy script is not executible. Skipping..."
-            fi
-        done
-    fi
-fi
+set -e
 
 echo "Running ansible_install_playbook.sh"
 
-echo "Using Ansible Path: $PLUGIN_DIR"
-for playbook in $(ls $PLUGIN_DIR/*.yaml || ls $PLUGIN_DIR/*.yml)
+path="$ENVROOT/ansible"
+echo "Using Ansible Path: $path"
+for playbook in $(ls $path/*.yaml || ls $path/*.yml)
 do
     echo "Executing playbook: $playbook"
     /root/.local/bin/ansible-playbook $playbook
