cffd2f8065c8f5d1bead249b7700d0556e17146b
dylan-bitovi <dylan@bitovi.com>
2020-07-16 12:34:31 -0400

    adding repo for mysql-client install

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -3,6 +3,7 @@ set -xe
 
 apt-get update -y && \
 add-apt-repository ppa:deadsnakes/ppa && \
+add-apt repostiory "deb [arch=amd64] http://archive.ubuntu.com/ubuntu $(lsb_release -cs) stable" && \
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
 apt-get update && \
 apt-get install -y software-properties-common libsodium-dev curl jq python3.7 mysql-client
