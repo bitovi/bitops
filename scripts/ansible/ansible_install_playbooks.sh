5bf86b88852d0ab746d27b09aad55d1baaa45517
Connor Graham <connor@bitovi.com>
2020-09-25 12:35:15 -0400

    ansible bitops schema

--- scripts/ansible/ansible_install_playbooks.sh
+++ scripts/ansible/ansible_install_playbooks.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+ANSIBLE_ARGS=$1
 PLUGIN_DIR="$ENVROOT/ansible"
 
 echo "Running ansible_install_playbook.sh for $PLUGIN_DIR"
 
 for playbook in $(ls $PLUGIN_DIR/*.yaml || ls $PLUGIN_DIR/*.yml); do
     echo "Executing playbook: $playbook"
-    ansible-playbook $playbook
+    ansible-playbook $playbook $ANSIBLE_ARGS
 done
