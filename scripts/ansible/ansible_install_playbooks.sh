356bc3b3c692c5b2926b72badc6bce637428b46c
Connor Graham <connor@bitovi.com>
2020-09-28 12:41:40 -0400

    ignore bitops.config from ansible playbook check

--- scripts/ansible/ansible_install_playbooks.sh
+++ scripts/ansible/ansible_install_playbooks.sh
@@ -6,7 +6,7 @@ PLUGIN_DIR="$ENVROOT/ansible"
 
 echo "Running ansible_install_playbook.sh for $PLUGIN_DIR"
 
-for playbook in $(ls $PLUGIN_DIR/*.yaml || ls $PLUGIN_DIR/*.yml); do
+for playbook in $(ls $PLUGIN_DIR/*[^bitops.config].yaml || ls $PLUGIN_DIR/*[^bitops.config].yml); do
     echo "Executing playbook: $playbook"
     ansible-playbook $playbook $ANSIBLE_ARGS
 done
