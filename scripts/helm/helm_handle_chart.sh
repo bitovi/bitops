f51843f9cec98d2dca32f1f535563a89c7643af4
Mick McGrath <mickmcgrath13@users.noreply.github.com>
2020-12-30 09:11:03 -0600

    handle HELM_SKIP_DEPLOY (#45)
    - handle HELM_SKIP_DEPLOY
    - fix bug in helm deployment where new version of helm history responds with error if no release found
    - fix bug with KUBE_CONFIG_PATH not setting KUBE_CONFIG_FILE properly

--- scripts/helm/helm_handle_chart.sh
+++ scripts/helm/helm_handle_chart.sh
@@ -19,13 +19,18 @@ on_exit () {
 }
 trap "{ on_exit; }" EXIT
 
-# Check for Before Deploy Scripts
-bash $SCRIPTS_DIR/deploy/before-deploy.sh "$HELM_CHART_DIRECTORY"
-
 # Load bitops.config.yaml
 export BITOPS_CONFIG_COMMAND="$(ENV_FILE="$BITOPS_SCHEMA_ENV_FILE" DEBUG="" bash $SCRIPTS_DIR/bitops-config/convert-schema.sh $BITOPS_CONFIG_SCHEMA $HELM_BITOPS_CONFIG)"
 echo "BITOPS_CONFIG_COMMAND: $BITOPS_CONFIG_COMMAND"
 source "$BITOPS_SCHEMA_ENV_FILE"
+
+if [ "$HELM_SKIP_DEPLOY" == "True" ]; then
+    echo "helm.options.skip-deploy (HELM_SKIP_DEPLOY) set.  Skipping deployment for $ENVIRONMENT/helm/$HELM_CHART"
+    exit 0
+fi
+
+# Check for Before Deploy Scripts
+bash $SCRIPTS_DIR/deploy/before-deploy.sh "$HELM_CHART_DIRECTORY"
 
 # set kube config
 if [[ "${KUBE_CONFIG_PATH}" == "" ]] || [[ "${KUBE_CONFIG_PATH}" == "''" ]] || [[ "${KUBE_CONFIG_PATH}" == "None" ]]; then
@@ -50,8 +55,8 @@ if [[ "${KUBE_CONFIG_PATH}" == "" ]] || [[ "${KUBE_CONFIG_PATH}" == "''" ]] || [
 else
     if [[ -f "$KUBE_CONFIG_PATH" ]]; then
         echo "$KUBE_CONFIG_PATH exists."
-        KUBE_CONFIG_FILE="$KUBE_CONFIG_PATH" \
-        KUBECONFIG="$KUBE_CONFIG_FILE" \
+        KUBE_CONFIG_FILE="$KUBE_CONFIG_PATH"
+        KUBECONFIG="$KUBE_CONFIG_FILE"
         export KUBECONFIG=$KUBECONFIG:$KUBE_CONFIG_FILE
     else
         >&2 echo "{\"error\":\"kubeconfig path variable wrong in bitops.config.yaml.Exiting...\"}"
