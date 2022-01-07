706d712b4319ff539f9174f42d7e658e6d047262
PhillypHenning <phillyp.henning@gmail.com>
2022-01-07 10:02:21 -0500

    removing comments

--- scripts/cloudformation/deploy.sh
+++ scripts/cloudformation/deploy.sh
@@ -116,8 +116,6 @@ if [[ -n $v ]]; then
     BITOPS_SCHEMA_ENV_FILE="$CLOUDFORMATION_ROOT_MULTIREGION/ENV_FILE"
     BITOPS_CONFIG_SCHEMA="$SCRIPTS_DIR/cloudformation/bitops.schema.yaml"
 
-    echo "$CLOUDFORMATION_ROOT_MULTIREGION    |    $CLOUDFORMATION_BITOPS_CONFIG    |    $BITOPS_SCHEMA_ENV_FILE    |    $BITOPS_CONFIG_SCHEMA"
-
     run_before_scripts
     run_config_conversion
     run_schema_validation
@@ -155,69 +153,3 @@ if [[ -n $v ]]; then
     run_after_scripts
 fi
 
-# # Check for Before Deploy Scripts
-# bash $SCRIPTS_DIR/deploy/before-deploy.sh "$CLOUDFORMATION_ROOT"
-
-
-# export BITOPS_CONFIG_COMMAND="$(ENV_FILE="$BITOPS_SCHEMA_ENV_FILE" DEBUG="" bash $SCRIPTS_DIR/bitops-config/convert-schema.sh $BITOPS_CONFIG_SCHEMA $CLOUDFORMATION_BITOPS_CONFIG)"
-# echo "BITOPS_CONFIG_COMMAND: $BITOPS_CONFIG_COMMAND"
-# echo "BITOPS_SCHEMA_ENV_FILE: $(cat $BITOPS_SCHEMA_ENV_FILE)"
-# source "$BITOPS_SCHEMA_ENV_FILE"
-
-# # Exit if Stack Name not found
-# if [[ "${CFN_STACK_NAME=}" == "" ]] || [[ "${CFN_STACK_NAME=}" == "''" ]] || [[ "${CFN_STACK_NAME=}" == "None" ]]; then
-#   >&2 echo "{\"error\":\"$CFN_STACK_NAME config is required in bitops config.Exiting...\"}"
-#   exit 1
-# fi
-
-# # Exit if CFN Template Filename is not found
-# if [[ "${CFN_TEMPLATE_FILENAME==}" == "" ]] || [[ "${CFN_TEMPLATE_FILENAME==}" == "''" ]] || [[ "${CFN_TEMPLATE_FILENAME==}" == "None" ]]; then
-#   >&2 echo "{\"error\":\"$CFN_TEMPLATE_FILENAME config is required in bitops config.Exiting...\"}"
-#   exit 1
-# fi
-
-# # Exit if CFN Template Parameters Filename is not found
-# if [[ "${CFN_PARAMS_FLAG}" == "True" ]] || [[ "${CFN_PARAMS_FLAG}" == "true" ]]; then
-#   if [[ "${CFN_TEMPLATE_PARAMS_FILENAME}" == "" ]] || [[ "${CFN_TEMPLATE_PARAMS_FILENAME}" == "''" ]] || [[ "${CFN_TEMPLATE_PARAMS_FILENAME}" == "None" ]]; then
-#     >&2 echo "{\"error\":\"$CFN_TEMPLATE_FILENAME config is required in bitops config.Exiting...\"}"
-#     exit 1
-#   fi
-# fi
-
-# echo "cd cloudformation Root: $CLOUDFORMATION_ROOT"
-# cd $CLOUDFORMATION_ROOT
-
-# # Combine parameters
-# if [[ "$CFN_MERGE_PARAMETER" == "true" ]] || [[ "$CFN_MERGE_PARAMETER" == "True" ]]; then
-#   echo "Combining json files in $CFN_MERGE_DIRECTORY folder"
-#   # All files in the $CFN_MERGE_DIRECTORY will be merged into the $CFN_TEMPLATE_PARAMS_FILENAME, if $CFN_TEMPLATE_PARAMS_FILENAME is unset it will use parameters.json
-#   COMBINE_FILES=
-#   for filename in $(ls $CLOUDFORMATION_ROOT/$CFN_MERGE_DIRECTORY); do
-#     COMBINE_FILES+="$CLOUDFORMATION_ROOT/$CFN_MERGE_DIRECTORY/$filename "
-#   done;
-#   jq '.[]' $COMBINE_FILES | jq -s . > $CFN_TEMPLATE_PARAMS_FILENAME
-# fi
-
-# # cloud provider auth
-# echo "cloudformation auth cloud provider"
-# bash $SCRIPTS_DIR/aws/sts.get-caller-identity.sh
-
-# always run cfn template validation first
-# if [[ "${CFN_TEMPLATE_VALIDATION}" == "True" ]] || [[ "${CFN_TEMPLATE_VALIDATION}" == "true" ]]; then
-#   echo "Running Cloudformation Template Validation"
-#   bash $SCRIPTS_DIR/cloudformation/cloudformation_validate.sh "$CFN_TEMPLATE_FILENAME"
-# fi
-
-# if [[ "${CFN_STACK_ACTION}" == "deploy" ]] || [[ "${CFN_STACK_ACTION}" == "Deploy" ]]; then
-#   echo "Running Cloudformation Deploy Stack"
-#   bash $SCRIPTS_DIR/cloudformation/cloudformation_deploy.sh "$CFN_TEMPLATE_FILENAME" "$CFN_PARAMS_FLAG" "$CFN_TEMPLATE_PARAMS_FILENAME" "$CFN_STACK_NAME" "$CFN_CAPABILITY" "$CFN_TEMPLATE_S3_BUCKET" "$CFN_S3_PREFIX"
-# fi
-
-# if [[ "${CFN_STACK_ACTION}" == "delete" ]] || [[ "${CFN_STACK_ACTION}" == "Delete" ]]; then
-#   echo "Running Cloudformation Delete Stack"
-#   bash $SCRIPTS_DIR/cloudformation/cloudformation_delete.sh "$CFN_STACK_NAME"
-# fi
-
-
-# Check for After Deploy Scripts
-# bash $SCRIPTS_DIR/deploy/after-deploy.sh "$CLOUDFORMATION_ROOT"
