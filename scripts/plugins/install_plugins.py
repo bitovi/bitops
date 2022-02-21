3c71862f7de2df65ce44775dd3d829fe348b7ce0
PhillypHenning <phillyp.henning@gmail.com>
2022-02-21 12:42:56 -0500

    initial pass with branch, tags and versions

--- scripts/plugins/install_plugins.py
+++ scripts/plugins/install_plugins.py
@@ -10,39 +10,54 @@ from .utilties import Load_Build_Config
 from munch import DefaultMunch
 
 def install_plugins():
-    MODE = os.environ.get("BITOPS_MODE", "debug")
-
     def git(*args):
         return subprocess.check_call(['git'] + list(args))
-
-    # print("Loading {}".format(config_file))
-    # # Load plugin config yml
-    # with open(config_file, 'r') as stream:
-    #     try:
-    #         plugins_yml = yaml.load(stream, Loader=yaml.FullLoader)
-    #     except yaml.YAMLError as exc:
-    #         print(exc)
-    #     except Exception as exc:
-    #         print(exc)
         
     plugins_yml = Load_Build_Config()
-    bitops_build_configuration = DefaultMunch.fromDict(plugins_yml, "bitops")
+    bitops_build_configuration = DefaultMunch.fromDict(plugins_yml, None)
     plugin_dir = "/opt/bitops/scripts/plugins/"
-    bitops_environment = bitops_build_configuration.bitops.environment
-    bitops_plugins = bitops_build_configuration.bitops.plugins
+    # bitops_environment = bitops_build_configuration.bitops.environment
+    bitops_plugins_configuration = DefaultMunch.fromDict(bitops_build_configuration.bitops.plugins.tools, None)
 
-    if bitops_plugins is None:
+    #print(bitops_build_configuration.bitops.plugins)
+    #print(bitops_build_configuration.bitops.plugins.tools)
+    #print(bitops_build_configuration.bitops.plugins.plugins_seq)
+
+    if bitops_plugins_configuration is None:
         print("No plugins found. Exiting {}".format(__file__))
         quit()
 
     # Loop through plugins and git clone each
-    for plugin in bitops_plugins:
+    for plugin in bitops_plugins_configuration:
         print("Preparing plugin: [{}]".format(plugin))
-        source = bitops_plugins[plugin].source 
+        source = bitops_plugins_configuration[plugin].source
+        download_logging_message = "Something went wrong during the plugin [{}] download".format(plugin)
+        
         if source is not None:
-            print("Downloading plugin: [{}], from: [{}]".format(plugin, source))
+            plugin_logging = "using branch: [master] "
+            install_tag = bitops_plugins_configuration[plugin].source_tag
+            install_branch = bitops_plugins_configuration[plugin].source_branch
+            
+            if install_branch is None and install_tag is None:
+                install_tag = "latest"
+                install_branch = "master"
+
+            elif install_branch is not None and install_tag is not None:
+                install_tag=""
+                plugin_logging = ", using branch: [{}]".format(install_branch)
+
+            else:
+                plugin_logging = ", using tag: [{}]".format(install_tag) if install_branch is None else ", using branch: [{}]".format(install_branch)
+
+
+            print("Downloading plugin: [{}], from: [{}], {}".format(plugin, source, plugin_logging))
             git("clone", source, plugin_dir + plugin)
-            print("Downloading complete")
+            download_logging_message = "Download completed"
+        else:
+            print("Plugin source cannot be empty. Plugin: [{}]".format(plugin))
+            download_logging_message = "Download did not run"
+        
+        print(download_logging_message)
 
 
         # install plugin dependencies (install.sh)
