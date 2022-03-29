8dc316c170d35b9582a71733ee2df4a3ef3f43be
Pablo Patino <pablo@bitovi.com>
2022-03-29 16:00:58 -0500

    Removed old references

--- docs/examples/bitops+fast+react+static+renderer/dev/terraform/route53.tf
+++ docs/examples/bitops+fast+react+static+renderer/dev/terraform/route53.tf
@@ -3,15 +3,3 @@ resource "aws_route53_zone" "main" {
   name = var.domain_name
   tags = var.common_tags
 }
-
-resource "aws_route53_record" "root-a" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = var.domain_name
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.root_s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
