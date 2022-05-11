c587f5df1447b770202a9dce1517ed1413e35eed
PhillypHenning <phillyp.henning@gmail.com>
2022-05-11 13:29:25 -0400

    Updating configuration and ENV var settings (X,Y,Z) (ENV VAR, CONFIG, DEFAULT)

--- scripts/plugins.py
+++ scripts/plugins.py
@@ -12,6 +12,19 @@ if __name__ == "__main__":
         mode = sys.argv[1]
     except IndexError:
         mode = None
+    
+    logger.info("\n\n\n~#~#~#~ BITOPS CONFIGURATION ~#~#~#~\
+    \n\tFAIL FAST: [{fail_fast}]\
+    \n\tRUN MODE: [{run_mode}]\
+    \n\tLOGGING LEVEL: [{log_level}]\
+    \n\tBITOPS SOURCE: [{github_source}]\
+    \n\n\n\
+    ".format(\
+        fail_fast=plugins.settings.BITOPS_fast_fail_mode, \
+        run_mode=plugins.settings.BITOPS_run_mode, \
+        log_level=plugins.settings.BITOPS_logging_level,\
+        github_source=plugins.settings.BITOPS_opsrepo_source\
+    ))
 
     if mode == "deploy":
         Deploy_Plugins()
--- scripts/plugins/settings.py
+++ scripts/plugins/settings.py
@@ -8,13 +8,38 @@ BITOPS_config_file = os.environ.get("BITOPS_BUILD_CONFIG_YAML", "bitops.config.y
 with open(BITOPS_config_file, 'r') as stream:    
     BITOPS_config_yaml = yaml.load(stream, Loader=yaml.FullLoader)
 
-# BitOps run options
-BITOPS_fast_fail_mode = os.environ.get("BITOPS_FAST_FAIL", True)
-BITOPS_run_mode = os.environ.get("BITOPS_MODE", "default") # ["default", "testing"]
-BITOPS_logging_level = os.environ.get("BITOPS_LOGGING_LEVEL", "INFO").upper
-
-
 # Updating from Bitops build config
 bitops_build_configuration = DefaultMunch.fromDict(BITOPS_config_yaml, None)
 bitops_build_configuration.bitops.fast_fail
-BITOPS_opsrepo_source = bitops_build_configuration.bitops.ops_repo.source
+
+
+
+# ENVIRONMENT
+BITOPS_ENV_fast_fail_mode = os.environ.get("BITOPS_FAST_FAIL")
+BITOPS_ENV_run_mode = os.environ.get("BITOPS_MODE")
+BITOPS_ENV_logging_level = os.environ.get("BITOPS_LOGGING_LEVEL")
+
+# WASHED VALUES
+# This is just stacked ternary operators. Don't be scared. All this does is X if X is set, Y if Y is set, else default value
+BITOPS_fast_fail_mode = BITOPS_ENV_fast_fail_mode                   \
+    if BITOPS_ENV_fast_fail_mode is not None                        \
+    else bitops_build_configuration.bitops.fail_fast                \
+        if bitops_build_configuration.bitops.fail_fast is not None  \
+        else True
+
+BITOPS_run_mode = BITOPS_ENV_run_mode                               \
+    if BITOPS_ENV_run_mode is not None                              \
+    else bitops_build_configuration.bitops.run_mode                 \
+        if bitops_build_configuration.bitops.run_mode is not None   \
+        else "default"
+
+BITOPS_logging_level = BITOPS_ENV_logging_level                         \
+    if BITOPS_ENV_logging_level is not None                             \
+    else bitops_build_configuration.bitops.logging.level                \
+        if bitops_build_configuration.bitops.logging.level is not None  \
+        else "DEBUG"
+
+BITOPS_opsrepo_source = bitops_build_configuration.bitops.ops_repo.source_from   \
+    if bitops_build_configuration.bitops.ops_repo.source_from is not None        \
+    else "local"
+
