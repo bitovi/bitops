0cda753ae863f9d6854d9c543b93cb3b8cbf6a97
PhillypHenning <phillyp.henning@gmail.com>
2022-05-11 18:53:57 -0400

    Using settings BITOPS_logging_level

--- scripts/plugins/logging.py
+++ scripts/plugins/logging.py
@@ -8,11 +8,13 @@ from .settings import BITOPS_logging_level
 # 3. WARN
 # 4. ERROR
 
+from .settings import BITOPS_logging_level
+
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger.setLevel(BITOPS_logging_level)
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
+handler.setLevel(BITOPS_logging_level)
 formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 
 handler.setFormatter(formatter)
