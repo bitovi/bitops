c86ae5f5f43085b1fc109fe52ca44d8e50ae3c1a
PhillypHenning <phillyp.henning@gmail.com>
2022-07-11 16:04:50 -0400

    Adding error catch

--- scripts/plugins/doc.py
+++ scripts/plugins/doc.py
@@ -7,6 +7,9 @@ from .logging import logger
 fh = open("scripts/plugins/documentation.json")
 jh = json.load(fh)
 
-def Get_Doc(doc_key):
-    msg = "\n\t{}\n\tFor more information checkout the Bitops Documentation: [{}]".format(jh[doc_key]["msg"], jh[doc_key]["link"])
+def Get_Doc(lookup_key):
+    try:
+        msg = "\n\t{}\n\tFor more information checkout the Bitops Documentation: [{}]".format(jh[lookup_key]["msg"], jh[lookup_key]["link"])
+    except KeyError:
+        return "DEVELOPER NOTE: Check lookup code and confirm that it is in the documentation config. Something has gone wrong."
     return msg
