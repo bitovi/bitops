4d75d41fca0c6cf6028d2dba69e4c9f6e0e411a4
PhillypHenning <phil8t8@gmail.com>
2022-10-12 16:29:09 -0400

    Adding return_number default

--- scripts/plugins/doc.py
+++ scripts/plugins/doc.py
@@ -1,5 +1,6 @@
 # Loads JSON
 # Provides a function to access the documentation
+from .logging import logger
 import json
 
 fh = open("scripts/plugins/documentation.json")
@@ -10,12 +11,20 @@ def Get_Doc(lookup_key):
     try:
         msg = "\n\t{}".format(jh[lookup_key]["msg"])
         link = jh[lookup_key]["link"]
+        return_number = 0
         if link:
             msg += "\n\tFor more information checkout the Bitops Documentation: [{}]".format(
                 link
             )
-        return_number = jh[lookup_key]["number"]
-
     except KeyError:
-        return "DEVELOPER NOTE: Check lookup code and confirm that it is in the documentation config. Something has gone wrong."
+        # Returns 1 by default, exit and report issue at developer level
+        return "DEVELOPER NOTE: Check lookup code and confirm that it is in the documentation config. Something has gone wrong.", 1
+    
+    try:
+        return_number = jh[lookup_key]["number"]
+        logger.debug(f"INC STRING: [{return_number}]")
+        if return_number: return_number = return_number
+    except KeyError:
+        return_number=0
+
     return msg, return_number
