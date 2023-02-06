e619210f2a607184e9238a5b1197b941d205e646
PhillypHenning <phil8t8@gmail.com>
2023-02-06 11:51:32 -0500

    Adding environment variable check, if found ignore bitops.config attribute w/warning

--- scripts/plugins/utilities.py
+++ scripts/plugins/utilities.py
@@ -176,6 +176,10 @@ def add_value_to_env(export_env, value):
     """
     if value is None or value == "" or value == "None" or export_env is None or export_env == "":
         return
+    
+    if os.environ.get('BITOPS_'+export_env):
+        logger.info("Environment Varible alredy set. Configuration value ignored.")
+        return
 
     if isinstance(value, bool):
         value = str(value).lower()
