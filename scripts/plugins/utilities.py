bitovi/bitops

commit fade109ef5bbf57d421c4b678fd7d83f77b322d7

Updating terraform doc with `targets` attribute (#395)
* Updating terraform doc

* Doc update to `terraform.target`attribute and adding `list_to_string` util function

* making formatter happy

* fixing plugin.source_target in schema

* Updating tool doc with `targets`

scripts/plugins/utilities.py
@@ -195,6 +195,13 @@ def add_value_to_env(export_env, value):
     if isinstance(value, bool):
         value = str(value).lower()
 
+    if isinstance(value, list):
+        value = " ".join(value)
+
+    export_env = "BITOPS_" + export_env
+    os.environ[export_env] = str(value)
+    logger.info(f"Setting environment variable: [{export_env}], to value: [{value}]")
+
     os.environ[export_env] = str(value)
     logger.info(f"Setting export environment variable: [{export_env}], to value: [{value}]")
 

bitops.schema.yaml
@@ -43,10 +43,10 @@ bitops:
                 sourced_from:
                   type: string
                   description: "Where the plugin will be sourced from"
-                branch: 
+                source_branch: 
                   type: string
                   description: "Github branch for versioned clone"
-                tag:
+                source_tag:
                   type: string
                   description: "Github tag for versioned clone"
     

commit 6b6c315b05049adabebb4db5b469ddced59365cb

Fix ENV export regression in #395

scripts/plugins/utilities.py
@@ -198,10 +198,6 @@ def add_value_to_env(export_env, value):
     if isinstance(value, list):
         value = " ".join(value)
 
-    export_env = "BITOPS_" + export_env
-    os.environ[export_env] = str(value)
-    logger.info(f"Setting environment variable: [{export_env}], to value: [{value}]")
-
     os.environ[export_env] = str(value)
     logger.info(f"Setting export environment variable: [{export_env}], to value: [{value}]")
 