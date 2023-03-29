7fc6697e9af968e551f5c2658c297ca60f85ab27
Eugen <eugen@bitovi.com>
2023-03-29 16:57:35 +0100

    Add a condition to skip the filename logging

--- scripts/plugins/logging.py
+++ scripts/plugins/logging.py
@@ -112,7 +112,7 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-if BITOPS_LOGGING_FILENAME:
+if BITOPS_LOGGING_FILENAME not in [None, "", "false"]:
     # This assumes that the user wants to save output to a filename
 
     # Create the directory if it doesn't exist
